062dbb023e846860122868ab53685ee05268e1fc
Tom Tobar <tomas.tobar@flatironschool.com>
2021-11-08 08:47:56 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,13 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id].to_i)
+  end
+
+  private 
+  def student_params
+    params.require(:students).permit(:id, :first_name, :last_name)
+  end
 end
